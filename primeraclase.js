a27e367efd5390ec8d9f2d33eecf4d0667105e54
danielgs29 <danielgs2929@gmail.com>
2022-10-14 19:23:18 +0200

    examples

--- primeraclase.js
+++ primeraclase.js
@@ -1,1 +1,27 @@
-//alert("I'm = \"I am\"\r\nI don\'t = \'I do not\'");s
+var a;
+var b;
+
+/*switch(a){
+    case 1:{
+        alert("case 1");
+        break;
+    }
+     case 2:{
+     alert("case 2");
+     break;
+    }
+     case 5:{
+        alert("case 5");
+        break
+    }
+    default:{
+        alert("default");
+    }
+}*/
+
+for(a=0,b=1; (a+b)<15; a++, b+=2){
+alert(a+b);
+}
+
+
+
